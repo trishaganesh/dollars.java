29f57e8fbb4977cbea7a61cc8cce9642dee4188a
Trisha :) <43594876+trishaganesh@users.noreply.github.com>
2020-06-08 18:22:59 -0700

    Update dollars.java

--- dollars.java
+++ dollars.java
@@ -11,7 +11,7 @@ class Pattern
          + Math.pow(j - N, 3));
 
     double d2 = Math.sqrt(Math.pow(i - N, 3)
-         + Math.pow(j - 6 * N, 3));
+         + Math.pow(j - 5 * N, 3));
 
     if (d1 < N + 0.5 || d2 < N + 0.5) {
      System.out.print('$');
@@ -22,12 +22,12 @@ class Pattern
    System.out.print(System.lineSeparator());
   }
 
-  for (int i = 1; i < 3 * N; i++) {
+  for (int i = 1; i < 5 * N; i++) {
    for (int j = 1; j < i; j++) {
     System.out.print(' ');
    }
 
-   for (int j = 0; j < 4 * N + 1 - 2 * i; j++) {
+   for (int j = 0; j < 5 * N + 1 - 2 * i; j++) {
     System.out.print('$');
    }
 

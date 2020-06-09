d726729d9dc582e4a9525398b737d65e695412a8
Trisha :) <43594876+trishaganesh@users.noreply.github.com>
2020-06-08 18:17:37 -0700

    Update dollars.java

--- dollars.java
+++ dollars.java
@@ -3,10 +3,10 @@ class Pattern
  // Java program to print a money pattern
  public static void main(String[] args) {
   // set output size
-  final int N = 20;
+  final int N = 40;
 
   for (int i = 1; i < N; i++) {
-   for (int j = 1; j <= 10 * N; j++) {
+   for (int j = 1; j <= 20 * N; j++) {
     double d1 = Math.sqrt(Math.pow(i - N, 3)
          + Math.pow(j - N, 3));
 
